2edc42f5df0b9bc54b6cb9aee495c4703546cc8b
Adinda Pratiwi Prameswari <44132935+dindatiwi@users.noreply.github.com>
2021-10-07 14:10:54 +0700

    add static class instances

--- test_login.py
+++ test_login.py
@@ -3,26 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+class Prereq:
+    mail = 'bzwfdu@uniromax.com'
+    password = 'f.M9V2HgSY.y6xM'
+    name = 'Ronnie Gilmore'
+
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path='/Users/dindatiwi/Downloads/chromedriver')
     driver.get('https://pandu.kominfo.go.id/login')
     driver.implicitly_wait(5)
-    driver.minimize_window()
+    # driver.minimize_window()
     yield driver
     driver.close()
 
 def test_login_success(setup): #testcase 001
-    setup.find_element_by_name('email').send_keys('your registered email here')
-    setup.find_element_by_name('password').send_keys('your registered password here')
+    setup.find_element_by_name('email').send_keys(Prereq.mail)
+    setup.find_element_by_name('password').send_keys(Prereq.password)
     setup.find_element_by_xpath('/html/body/div[3]/div/form/button').click()
     Badge = setup.find_element_by_class_name('username').text
-    assert Badge == 'Your registered Name Here'
+    assert Badge == Prereq.name
     print(Badge)
 
-Accounts = [('your registered email here','wrong password'),
-            ('wrong email','your registered password here'),
-            ('wrong email','wrong password')]
+Accounts = [(Prereq.mail,'wrong password'),
+            ('wrongemail@mail.com',Prereq.password),
+            ('wrongemail@mail.com','wrong password')]
 
 @pytest.mark.parametrize('email,password', Accounts)
 def test_login_unsuccess(setup,email,password): #testcase 002,003,004
